6b7502563e5dc31dee576bedb2ec33e1d0877d09
Mario1870 <marioraach01@gmail.com>
2023-06-04 00:59:52 +0200

    asffed

--- src/app/api/get_all_teams/route.js
+++ src/app/api/get_all_teams/route.js
@@ -5,11 +5,12 @@ import prisma from "../../../../lib/prisma"
 import { NextResponse, NextRequest } from 'next/server';
 import { sql } from "@vercel/postgres";
 
+
 export async function GET(req, res){
 
     // insert into prisma  =>  {where:{gender: "m"}}
     try {
-      const teams = await prisma.Teams.findMany();
+      const teams = await prisma.Teams.deleteMany();
       return NextResponse.json(teams);;
     } catch (error) {
       console.error(error);
