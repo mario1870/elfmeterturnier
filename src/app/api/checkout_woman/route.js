138d84f2723cfb1dc9769296cf9d8e2b9ed3ad29
Mario1870 <marioraach01@gmail.com>
2023-06-05 22:58:10 +0200

    test_price_woman

--- src/app/api/checkout_woman/route.js
+++ src/app/api/checkout_woman/route.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(request) {
 
-  const priceId = 'price_1NFiNFCI8cvk3GBMGOikfuoV';
+  const priceId = 'price_1NFkQVCI8cvk3GBMBPqSZVth';
   const url = 'https://elfmeterturnier-melchingen.vercel.app'; 
 
   const chunks = [];
