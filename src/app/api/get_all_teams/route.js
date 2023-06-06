07afe85f2d89823270ef18fb2a90d5504afd9c42
Mario1870 <marioraach01@gmail.com>
2023-06-06 22:19:14 +0200

    06.06

--- src/app/api/get_all_teams/route.js
+++ src/app/api/get_all_teams/route.js
@@ -1,9 +1,6 @@
 
-
-
 import prisma from "../../../../lib/prisma"
 import { NextResponse, NextRequest } from 'next/server';
-import { sql } from "@vercel/postgres";
 
 
 export async function GET(req, res){
