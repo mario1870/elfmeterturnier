ccb8e0ed2d9de7b5c286c52c1e2e92417fb03c08
Mario1870 <marioraach01@gmail.com>
2023-06-07 23:37:00 +0200

    pushch

--- src/app/components/Homepage_link.jsx
+++ src/app/components/Homepage_link.jsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 export default function Homepage_link(){
     return(
         <div className="absolute top-2 left-2 filter invert opacity-60">
-            <Link href="https://www.tv-melchingen.de/"><Image src="/arrow.png" width={50} height={50}></Image></Link>
+            <Link href="https://www.tv-melchingen.de/"><Image alt="Zurück zur Homepage" src="/arrow.png" width={50} height={50}></Image></Link>
         </div>
     )
 }
