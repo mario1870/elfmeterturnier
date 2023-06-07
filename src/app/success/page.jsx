aa21a827560c1a1c98a3bc125d06f73556c91cfd
Mario1870 <marioraach01@gmail.com>
2023-06-08 01:40:49 +0200

    lol

--- src/app/success/page.jsx
+++ src/app/success/page.jsx
@@ -12,7 +12,7 @@ export default function Home() {
           <Image src="/OhIV.gif" width={400} height={400} alt='GIF' />
           <h1 className='text-white text-center text-2xl mt-20'>Bis zum 04.08. 👋</h1>   
         </div>
-        <h3 className='text-white font-bold absolute bottom-8 w-screen left-0 text-center'><Link href="/" >Zurück zur Anmeldeseite!</Link></h3>
+        <h3 className='text-white font-bold absolute bottom-8 w-screen left-0 text-center'><Link href="/" >Zurück zur Anmeldeseite</Link></h3>
     </div>
     </>
   )
